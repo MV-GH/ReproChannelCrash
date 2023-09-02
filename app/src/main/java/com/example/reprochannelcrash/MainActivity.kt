c09f5e023613391159b8f7c14ec0af695748dc2e
maarten.vercruysse <maarten.vercruysse@outlook.be>
2023-09-03 00:45:08 +0200

    Add RTL bug repro

--- app/src/main/java/com/example/reprochannelcrash/MainActivity.kt
+++ app/src/main/java/com/example/reprochannelcrash/MainActivity.kt
@@ -1,22 +1,20 @@
 package com.example.reprochannelcrash
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.WindowInsetsControllerCompat
 import com.example.reprochannelcrash.ui.theme.ReproChannelCrashTheme
 import me.saket.telephoto.zoomable.coil.ZoomableAsyncImage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL;
         super.onCreate(savedInstanceState)
         setContent {
             ReproChannelCrashTheme {
@@ -27,7 +25,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     ZoomableAsyncImage(
                         contentScale = ContentScale.Fit,
-                        model = "https://media.rimworld.gallery/0b/00/0b005a6f66489ac779870a55e05ab0f290e7a679c6f6c35fb289a73a71b7d584.jpg",
+                        model = "https://programming.dev/pictrs/image/24f030a2-2a1c-4733-a844-5be5cc99d9ef.png",
                         contentDescription = null,
                         modifier = Modifier.fillMaxSize(),
                     )
