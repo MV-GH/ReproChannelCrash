76f2ac0afba17c632839546c2365152630f16776
maarten.vercruysse <maarten.vercruysse@outlook.be>
2023-09-04 12:26:15 +0200

    Update telephoto version

--- app/src/main/java/com/example/reprochannelcrash/MainActivity.kt
+++ app/src/main/java/com/example/reprochannelcrash/MainActivity.kt
@@ -6,14 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.WindowInsetsControllerCompat
 import com.example.reprochannelcrash.ui.theme.ReproChannelCrashTheme
+import me.saket.telephoto.zoomable.ZoomSpec
 import me.saket.telephoto.zoomable.coil.ZoomableAsyncImage
+import me.saket.telephoto.zoomable.rememberZoomableImageState
+import me.saket.telephoto.zoomable.rememberZoomableState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,9 +24,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
+                    val zoomableState = rememberZoomableState(ZoomSpec(2F))
+                    val zoomableImageState = rememberZoomableImageState(zoomableState)
+
+
                     ZoomableAsyncImage(
+                        state = zoomableImageState,
                         contentScale = ContentScale.Fit,
-                        model = "https://media.rimworld.gallery/0b/00/0b005a6f66489ac779870a55e05ab0f290e7a679c6f6c35fb289a73a71b7d584.jpg",
+                        model = "https://lemmy.ml/pictrs/image/415c44c0-d626-4744-a026-75adeb9f6123.webp",
                         contentDescription = null,
                         modifier = Modifier.fillMaxSize(),
                     )
